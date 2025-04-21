07ea4ee323b0ddc61f1eef513dde1f24e9167107
hredoy.sen <hredoy.sen@bracits.com>
2025-04-21 16:42:14 +0600

    feat: all the basic changes have been made

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
     standalone: true,
     selector: 'app-footer',
     template: `<div class="layout-footer">
-        SAKAI by
-        <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeNG</a>
+        Angular Template by
+        <a href="https://hredoysen.netlify.app/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">HreduBro</a>
     </div>`
 })
 export class AppFooter {}
